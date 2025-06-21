2758baf04f37b7db406e799ea25dea8ce857a7c6
Acer <blvck2371>
2025-06-21 10:55:43 +0200

    test

--- lib/view/Home.dart
+++ lib/view/Home.dart
@@ -192,7 +192,7 @@ class _HomeState extends State<Home> {
                   ),
                   const SizedBox(width: 8),
                   Text(
-                    'Déconnexion',
+                    'Déconnexioner',
                     style: TextStyle(
                       color: Theme.of(context).colorScheme.error,
                     ),
